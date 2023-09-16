26462f58b4b5e4401555b722c8cda29b1dfb55b8
saiyagodvp <nambpph24550@fpt.edu.vn>
2023-09-16 20:56:28 +0700

    no message

---
+++ src/main/java/com/example/nhom5thuctapfall2023/Nhom5ThucTapFall2023Application.java
@@ -0,0 +1,13 @@
+package com.example.nhom5thuctapfall2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom5ThucTapFall2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nhom5ThucTapFall2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nhom5thuctapfall2023/Nhom5ThucTapFall2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nhom5thuctapfall2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom5ThucTapFall2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
